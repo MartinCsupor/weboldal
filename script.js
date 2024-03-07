c45c1533585af988f15f585a5b830dab67da99dc
MartinCsupor <csupormartin@turr.hu>
2024-03-07 09:35:51 +0100

    afa

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("ar1").innerHTML = "65 000Ft";
+}
+
+function myFunction1() {
+    document.getElementById("ar1").innerHTML = "75 000Ft";
+}
+
+function myFunction2() {
+    document.getElementById("ar1").innerHTML = "85 000Ft";
+}

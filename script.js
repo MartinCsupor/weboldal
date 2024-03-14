04d8ffe0186a1258700cca02b6d9211216842d80
MartinCsupor <csupormartin@turr.hu>
2024-03-14 09:40:27 +0100

    gagag

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function myFunction1() {
 function myFunction2() {
     document.getElementById("ar1").innerHTML = "85 000Ft";
 }
+
+document.addEventListener('DOMContentLoaded', nav)
+function nav(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+    })
+}
